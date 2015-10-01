13c5f7e8daf4e68d2e07dfdfe202fb46c9d0abee
Yanhe <yanhe.liu@cs.helsinki.fi>
2015-10-01 16:08:46 +0300

    Change php script with new REST-API urls

--- demo-web/libs/php/agents-wrapper.php
+++ demo-web/libs/php/agents-wrapper.php
@@ -1,6 +1,6 @@
 <?php
   header('Content-type: application/json');
-  $url = 'http://127.0.0.1:8080/wm/softoffload/agent/json';
+  $url = 'http://127.0.0.1:8080/wm/softoffload/agents/json';
   $json = file_get_contents($url);
 
   echo $json;
